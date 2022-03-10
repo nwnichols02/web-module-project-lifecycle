811f43d0e6a059e1bafc76189f8184b5610f7ced
Nathan Nichols <nwnichols02@gmail.com>
2022-03-10 08:20:30 -0700

    single network request per submit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,30 +6,37 @@ const URL = `http://localhost:9000/api/todos`;
 export default class App extends React.Component {
   state = {
     todos: [],
-    error: '',
-    todoNameInput: '',
+    error: "",
+    todoNameInput: "",
   };
 
-  onTodoNameInputChange = evt => {
-    const {value} = evt.target
-    this.setState({...this.state, todoNameInput: value})
-  }
+  onTodoNameInputChange = (evt) => {
+    const { value } = evt.target;
+    this.setState({ ...this.state, todoNameInput: value });
+  };
+
+  resetForm = () => {
+    this.setState({ ...this.state, todoNameInput: "" });
+  };
+
+  setAxiosResponseError = (err) => {
+    this.setState({ ...this.state, error: err.response.data.message });
+  };
 
   postNewTodo = () => {
-    axios.post(URL, {name: this.state.todoNameInput})
-    .then(res => {
-      this.fetchAllTodos();
-      this.setState({...this.state, todoNameInput: ''})
-    })
-    .catch(res => {
-      this.setState({...this.state, error: res.response.data.message})
-    })
-  }
+    axios
+      .post(URL, { name: this.state.todoNameInput })
+      .then((res) => {
+        this.setState({...this.state, todos: this.state.todos.concat(res.data.data)})
+        this.resetForm();
+      })
+      .catch(this.setAxiosResponseError);
+  };
 
   onTodoFormSubmit = (evt) => {
     evt.preventDefault();
     this.postNewTodo();
-  }
+  };
 
   fetchAllTodos = () => {
     axios
@@ -37,9 +44,7 @@ export default class App extends React.Component {
       .then((res) => {
         this.setState({ ...this.state, todos: res.data.data });
       })
-      .catch((err) => {
-        this.setState({ ...this.state, error: err.response.data.message });
-      });
+      .catch(this.setAxiosResponseError);
   };
   componentDidMount() {
     //fetch all todos from server.
@@ -56,7 +61,12 @@ export default class App extends React.Component {
           })}
         </div>
         <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
-          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder="Type todo"></input>
+          <input
+            value={this.state.todoNameInput}
+            onChange={this.onTodoNameInputChange}
+            type="text"
+            placeholder="Type todo"
+          ></input>
           <input type="submit"></input>
           <button>Clear Completed</button>
         </form>
