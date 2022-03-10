f4b8cdefc06ce94be992710e02c7cc578d49bb6f
Nathan Nichols <nwnichols02@gmail.com>
2022-03-10 08:51:45 -0700

    hide and show completed functioning

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,6 +8,7 @@ export default class App extends React.Component {
     todos: [],
     error: "",
     todoNameInput: "",
+    displayCompleteds: true,
   };
 
   onTodoNameInputChange = (evt) => {
@@ -27,7 +28,10 @@ export default class App extends React.Component {
     axios
       .post(URL, { name: this.state.todoNameInput })
       .then((res) => {
-        this.setState({...this.state, todos: this.state.todos.concat(res.data.data)})
+        this.setState({
+          ...this.state,
+          todos: this.state.todos.concat(res.data.data),
+        });
         this.resetForm();
       })
       .catch(this.setAxiosResponseError);
@@ -46,6 +50,29 @@ export default class App extends React.Component {
       })
       .catch(this.setAxiosResponseError);
   };
+
+  toggleCompleted = (id) => () => {
+    axios
+      .patch(`${URL}/${id}`)
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          todos: this.state.todos.map((td) => {
+            if (td.id !== id) return td;
+            return res.data.data;
+          }),
+        });
+      })
+      .catch(this.setAxiosResponseError);
+  };
+
+  toggleDisplayCompleteds = () => {
+    this.setState({
+      ...this.state,
+      displayCompleteds: !this.state.displayCompleteds,
+    });
+  };
+
   componentDidMount() {
     //fetch all todos from server.
     this.fetchAllTodos();
@@ -56,9 +83,21 @@ export default class App extends React.Component {
         <div id="error"> Error: {this.state.error}</div>
         <div id="todos">
           <h2>Todos:</h2>
-          {this.state.todos.map((td) => {
-            return <div key={td.id}>{td.name}</div>;
-          })}
+          {this.state.todos.reduce((acc, td) => {
+            if (this.state.displayCompleteds || !td.completed)
+              return acc.concat(
+                <div onClick={this.toggleCompleted(td.id)} key={td.id}>
+                  {td.name} {td.completed ? " ✓" : ""}{" "}
+                </div>
+              );
+            return acc;
+          }, [])}
+
+          {/* return (
+              
+                {td.name} {td.completed ? " ✓" : ""}{" "}
+              </div>
+            ); */}
         </div>
         <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
           <input
@@ -68,8 +107,10 @@ export default class App extends React.Component {
             placeholder="Type todo"
           ></input>
           <input type="submit"></input>
-          <button>Clear Completed</button>
         </form>
+        <button onClick={this.toggleDisplayCompleteds}>
+          {this.state.displayCompleteds ? "Hide" : "Show"} Completed
+        </button>
       </div>
     );
   }
