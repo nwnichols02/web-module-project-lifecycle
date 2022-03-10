d35cbb9c586933ebf7d44bbbdaf3387cb6bd4e10
Nathan Nichols <nwnichols02@gmail.com>
2022-03-10 07:45:22 -0700

    got todos from api

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,107 +1,42 @@
-import React from 'react';
+import React from "react";
 import axios from 'axios';
-import ToDoList from './TodoList';
-import Form from './Form';
 
-const URL = 'http://localhost:9000/api/todos'
-
-const initialState = {
-  input: '',
-  todos: [],
-}
+const URL = `http://localhost:9000/api/todos`
 
 export default class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = initialState;
+  state = {
+    todos: [],
   }
-
-  componentDidMount() {
-    this.getTodos();
-  }
-
-  getTodos = () => {
+  fetchAllTodos = () => {
     axios.get(URL)
     .then(res => {
-      console.log(res.data.data)
-      this.setState({
-        ...this.state,
-        task: res.data.data,
-
-      })
-    })
-    .catch(err => console.error('she broke', err))
-  }
-
-
-//delete 
-  handleClear = () => {
-    this.setState({
-      ...this.state,
-      todos: this.state.todos.filter((todo) => {
-        return todo.completed === false;
-      }),
-    });
-  };
-//post to add to list
-  // handleAdd = (task) => {
-  //   const newTodo = {
-  //     task: task,
-  //     id: Date.now(),
-  //     completed: false,
-  //   }
-
-  //   this.setState({
-  //     ...this.state,
-  //     todos: [...this.state.todos, newTodo]
-  //   })
-  // }
-  handleAdd = () => {
-    const newTodo = {
-      task: this.state.data.data.name,
-      completed: this.state.data.completed,
-    }
-    axios.post(URL, newTodo)
-    .then(res => {
-      this.setState({
-        ...this.state,
-        todos: [...this.state.todos, res.data.name]
-      })
+      this.setState({...this.state, todos: res.data.data})
     })
     .catch(err => {
-      console.error(err)
-      this.setState({ ...this.state,
-        errorMessage: err.response.data.message
-      })
+      debugger
     })
   }
-//transform to Patch
-  handleToggle = (clickedId) => {
-
-    this.setState({
-      ...this.state,
-      todos: this.state.todos.map((todo) => {
-        if(todo.id === clickedId){
-          return {
-            ...todo,
-            completed: !todo.completed
-          }
-        } 
-        return todo;
-      })
-    })
+  componentDidMount(){
+    //fetch all todos from server.
+    this.fetchAllTodos()
   }
-
   render() {
-    const { todos } = this.state;
     return (
       <div>
-        <h2>Todo List:</h2>
-        {/* is this correct */}
-        <li>{this.state.todos}</li>
-        <ToDoList handleToggle={this.handleToggle} todos={todos} />
-        <Form handleAdd={this.handleAdd}/>
-        <button onClick={this.handleClear}>Clear</button>
+        <div id="error"> Error: no error here</div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
+        </div>
+        <form id="todoForm">
+          <input type="text" placeholder="Type todo"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
       </div>
     );
   }
