2d66ebbf8ad61f97e2245ec2adeffce8737f10cd
Nathan Nichols <nwnichols02@gmail.com>
2022-03-10 07:50:20 -0700

    error functioning

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,36 +1,36 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 
-const URL = `http://localhost:9000/api/todos`
+const URL = `http://localhost:9000/api/todos`;
 
 export default class App extends React.Component {
   state = {
     todos: [],
-  }
+    error: '',
+  };
   fetchAllTodos = () => {
-    axios.get(URL)
-    .then(res => {
-      this.setState({...this.state, todos: res.data.data})
-    })
-    .catch(err => {
-      debugger
-    })
-  }
-  componentDidMount(){
+    axios
+      .get(URL)
+      .then((res) => {
+        this.setState({ ...this.state, todos: res.data.data });
+      })
+      .catch((err) => {
+        this.setState({ ...this.state, error: err.response.data.message });
+      });
+  };
+  componentDidMount() {
     //fetch all todos from server.
-    this.fetchAllTodos()
+    this.fetchAllTodos();
   }
   render() {
     return (
       <div>
-        <div id="error"> Error: no error here</div>
+        <div id="error"> Error: {this.state.error}</div>
         <div id="todos">
           <h2>Todos:</h2>
-          {
-            this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
-            })
-          }
+          {this.state.todos.map((td) => {
+            return <div key={td.id}>{td.name}</div>;
+          })}
         </div>
         <form id="todoForm">
           <input type="text" placeholder="Type todo"></input>
